07d18ad3b7aa15f27aa6ceca1722c035f22561b9
Marc Pignat <marc@pignat.org>
2021-03-24 09:37:59 +0100

    fix logrorate permissions

--- release.sh
+++ release.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# git don't stores r/w permissions, so let's fix that
+chmod g-w target/linux/brcm2708/base-files/etc/logrotate.conf
+chmod g-w target/linux/brcm2708/base-files/etc/logrotate.d/z-way-server
+
 make -j9 || exit
 DATE=$(date -I)
 cp ./bin/targets/brcm2708/bcm2709-glibc/openwrt-brcm2708-bcm2709-rpi-2-squashfs-sysupgrade.img.gz "domos-openwrt-$DATE-sysupgrade.img.gz"
