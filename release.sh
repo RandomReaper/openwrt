229dfcd08956198d3331ed624e93635f104d7ca9
Marc Pignat <marc@pignat.org>
2021-05-24 09:36:44 +0200

    add /etc/domos-release file

--- release.sh
+++ release.sh
@@ -4,7 +4,9 @@
 chmod g-w target/linux/brcm2708/base-files/etc/logrotate.conf
 chmod g-w target/linux/brcm2708/base-files/etc/logrotate.d/z-way-server
 
-make -j9 || exit
 DATE=$(date -I)
+mkdir -p files/etc/
+echo -n "$DATE"> files/etc/domos-release
+make -j9 || exit
 cp ./bin/targets/brcm2708/bcm2709-glibc/openwrt-brcm2708-bcm2709-rpi-2-squashfs-sysupgrade.img.gz "domos-openwrt-$DATE-sysupgrade.img.gz"
 cp ./bin/targets/brcm2708/bcm2709-glibc/openwrt-brcm2708-bcm2709-rpi-2-squashfs-factory.img.gz "domos-openwrt-$DATE-factory.img.gz"
