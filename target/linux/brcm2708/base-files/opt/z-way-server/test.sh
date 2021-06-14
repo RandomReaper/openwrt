ab558eadb44e0d9be52cdff236c04c5cab1950cb
Marc Pignat <marc@pignat.org>
2021-06-14 09:13:34 +0200

    fix z-way-server config test

--- target/linux/brcm2708/base-files/opt/z-way-server/test.sh
+++ target/linux/brcm2708/base-files/opt/z-way-server/test.sh
@@ -2,22 +2,16 @@
 FILE=/opt/z-way-server/automation/storage/configjson-06b2d3b23dce96e1619d2b53d6c947ec.json
 ORIG=/rom/"$FILE"
 
-logger "testing z-way-server configuration file"
-
-if [ -e "$FILE" ]
+if [ ! -e "$FILE" ]
 then
-	logger "config file exists"
-else
-	logger "config file not found, get backup"
+	logger "z-way-server config file not found, get backup"
 	cp "$ORIG" "$FILE"
 	exit 1
 fi
 
-if grep -q "ttyUSB0" "$FILE"
+if ! grep -q "ttyUSB0" "$FILE"
 then
-	logger "config seems OK"
-else
-	logger "config file wrong, get backup"
+	logger "z-way-server config file wrong, get backup"
         cp "$ORIG" "$FILE"
 	exit 1
 fi
